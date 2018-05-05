e7c1807288e4bb52630e45ba92780c68b2335e4b
Andrei Cioara <andrei@cioara.me>
2018-05-05 13:12:20 +0100

    Added templates

---
+++ templates/codejam.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <limits>
+#include <cassert>
+#include <unordered_map>
+
+using namespace std;
+
+#define e '\n'
+#define INF 1023456789
+#define ll long long
+#define ull unsigned long long
+
+#define gg(x) cerr << #x << ": " << x << endl;
+
+//#define FILE "data"
+
+#ifdef FILE
+ifstream f(string (string(FILE) + ".in").c_str());
+ofstream g(string (string(FILE) + ".out").c_str());
+#endif
+#ifndef FILE
+#define f cin
+#define g cout
+#endif
+
+#ifdef CLION
+#undef f
+#undef g
+ifstream f("data.in");
+#define g cout
+#endif
+
+ll mul_inv(ll a, ll b) {
+  ll b0 = b, t, q;
+  ll x0 = 0, x1 = 1;
+  if (b == 1) return 1;
+  while (a > 1) {
+    q = a / b;
+    t = b, b = a % b, a = t;
+    t = x0, x0 = x1 - q * x0, x1 = t;
+  }
+  if (x1 < 0) x1 += b0;
+  return x1;
+}
+
+class Compare {
+public:
+    bool operator() (ll & first, ll & second) {
+        return first < second;
+    }
+};
+
+
+int main() {
+	ll t;
+	f >> t;
+
+	for (int ii = 1; ii <= t; ii++) {
+        ll n;
+		f >> n;
+
+		g << "Case #" << ii << ": ";
+		g << n;
+		g << e;
+	}
+
+	return 0;
+}
